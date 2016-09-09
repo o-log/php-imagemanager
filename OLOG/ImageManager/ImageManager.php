a0741a968d0b95053853ea0fe99536f1fbfcd510
D.Tsaritov <tsaritov@gmail.com>
2016-09-09 10:16:51 +0300

    Cache-control: max-age=180000000

--- OLOG/ImageManager/ImageManager.php
+++ OLOG/ImageManager/ImageManager.php
@@ -43,7 +43,8 @@ class ImageManager
         $ext = pathinfo($fullpath, PATHINFO_EXTENSION);
 
         $fp = fopen($fullpath, 'rb');
-        header("Cache-control: max-age=180000000");
+        header("Cache-Control:public");
+        header("Cache-Control:max-age=315360000");
         header("Content-Type: image/" . $ext);
         header("Content-Length: " . filesize($fullpath));
         fpassthru($fp);
