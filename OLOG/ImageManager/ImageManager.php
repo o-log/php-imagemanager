bca6bb21cf546e957aa3e130187e9f2d7bf489ac
D.Tsaritov <tsaritov@gmail.com>
2016-09-20 16:16:43 +0300

    one string header

--- OLOG/ImageManager/ImageManager.php
+++ OLOG/ImageManager/ImageManager.php
@@ -43,7 +43,7 @@ class ImageManager
         $ext = pathinfo($fullpath, PATHINFO_EXTENSION);
 
         $fp = fopen($fullpath, 'rb');
-        header("Cache-Control:public,max-age=315360000", false);
+        header("Cache-Control:public,max-age=315360000");
         header("Content-Type: image/" . $ext);
         header("Content-Length: " . filesize($fullpath));
         fpassthru($fp);
